db5a307f0bd4a0132d6c235f47e429262d8ea424
alimovj <youremail@example.com>
2025-04-19 11:29:24 +0500

    🔥 Optimized eager loading with loadMissing

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 Rename
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renam
     }
 
     public function index()
