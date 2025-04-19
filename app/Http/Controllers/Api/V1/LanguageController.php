7af261d295b40848b5d416c12d3e64d313ecae46
alimovj <youremail@example.com>
2025-04-19 11:25:14 +0500

    🌍 Added Russian translation file

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image folder t
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image folder 
     }
 
     public function index()
