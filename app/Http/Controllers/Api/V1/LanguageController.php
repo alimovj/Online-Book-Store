22de1e1cffce3eec9bd6c70dd444b3a953e450f0
alimovj <youremail@example.com>
2025-04-19 11:30:01 +0500

    ✨ Added BookFactory for seeding

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 Ren
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Re
     }
 
     public function index()
