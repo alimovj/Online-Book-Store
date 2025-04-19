3a18706b618296ef14e338585aedf3ae299fe5e3
alimovj <youremail@example.com>
2025-04-19 11:25:41 +0500

    🔄 Localized error messages

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image folde
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image fold
     }
 
     public function index()
