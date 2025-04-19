ad41cf1bf9153e92d79d66b138306efed0041ac5
alimovj <youremail@example.com>
2025-04-19 11:25:28 +0500

    🌍 Added English translation fallback

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image folder 
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image folde
     }
 
     public function index()
