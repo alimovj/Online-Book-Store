3e9f406669518d45b0f84818df7a0a8f248e0e5a
alimovj <youremail@example.com>
2025-04-19 11:30:26 +0500

    🧪 Wrote API test for book show endpoint

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 R
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 
     }
 
     public function index()
