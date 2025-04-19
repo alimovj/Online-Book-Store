e35904255494060479e130b6baff6e460347805e
alimovj <youremail@example.com>
2025-04-19 11:26:43 +0500

    📥 Added file_exists check before delete

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image 
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed imag
     }
 
     public function index()
