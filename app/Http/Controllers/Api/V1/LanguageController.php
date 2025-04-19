5c9e05c95e821db76ee563c85a70ee5bb5e9b3b0
alimovj <youremail@example.com>
2025-04-19 11:24:32 +0500

    ✏️ Fixed typos in validation messages

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin']);
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image folder to 'books'
     }
 
     public function index()
