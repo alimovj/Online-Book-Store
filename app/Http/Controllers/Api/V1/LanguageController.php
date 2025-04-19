4b612dacd294558695e3646a2d8a021207c9e5b0
alimovj <youremail@example.com>
2025-04-19 11:26:15 +0500

    🧽 Added docblocks to controller methods

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image fo
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed image f
     }
 
     public function index()
