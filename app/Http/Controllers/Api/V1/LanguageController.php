625c4dc2dc52a3bc311ff54f9a8bd6c697c9361e
alimovj <youremail@example.com>
2025-04-19 11:29:36 +0500

    🔄 Created BookPolicy for access control

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renam
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Rena
     }
 
     public function index()
