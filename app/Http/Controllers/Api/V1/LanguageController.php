274da8ed3389c08b9b0fea6b1672ee4034d33520
alimovj <youremail@example.com>
2025-04-19 11:28:14 +0500

    ⚙️ Configured disk name using config()

--- app/Http/Controllers/Api/V1/LanguageController.php
+++ app/Http/Controllers/Api/V1/LanguageController.php
@@ -14,7 +14,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed imag
+        $this->middleware(['auth:sanctum', 'role:admin'])📥 Renamed ima
     }
 
     public function index()
